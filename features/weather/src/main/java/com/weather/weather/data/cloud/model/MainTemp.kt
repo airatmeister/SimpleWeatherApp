d3311b113d374a17e173c823767da9bd0bcc1676
airatmeister <zadrot.tat.99@gmail.com>
2022-08-25 14:10:10 +0300

    initial commit

--- features/weather/src/main/java/com/weather/weather/data/cloud/model/MainTemp.kt
+++ features/weather/src/main/java/com/weather/weather/data/cloud/model/MainTemp.kt
@@ -3,6 +3,7 @@ package com.weather.weather.data.cloud.model
 import com.google.gson.annotations.SerializedName
 
 class MainTemp {
+
     @SerializedName("temp")
     var temp: Float = 0F
 }
--- features/weather/src/main/java/com/weather/weather/data/cloud/model/Temp.kt
+++ features/weather/src/main/java/com/weather/weather/data/cloud/model/Temp.kt
@@ -3,6 +3,7 @@ package com.weather.weather.data.cloud.model
 import com.google.gson.annotations.SerializedName
 
 class Temp {
+
     @SerializedName("main")
     var main: MainTemp? = null
 }
